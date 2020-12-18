de13a8f2cc8d00831827377bdd7ea0252deb82d0
白云辉 <kooksee@163.com>
2020-12-18 10:52:34 +0800

    update dix trace

--- dix_trace/trace.go
+++ dix_trace/trace.go
@@ -4,23 +4,13 @@ import (
 	"expvar"
 
 	"github.com/pubgo/dix"
-	"github.com/pubgo/dix/dix_envs"
 	"github.com/pubgo/xerror"
 )
 
 type TraceCtx struct{ dix.Model }
 
-func Trace() error {
-	return dix.Dix(TraceCtx{})
-}
-
-func With(fn func(_ *TraceCtx)) {
-	if dix_envs.IsTrace() {
-		return
-	}
-
-	xerror.Next().Panic(dix.Dix(fn))
-}
+func Trace() error              { return xerror.Wrap(dix.Dix(TraceCtx{})) }
+func With(fn func(_ *TraceCtx)) { xerror.Next().Panic(dix.Dix(fn)) }
 
 func init() {
 	With(func(_ *TraceCtx) { expvar.NewString("dix").Set(dix.Graph()) })
--- version/version.go
+++ version/version.go
@@ -1,4 +1,4 @@
 package version
 
-const BuildTime = "2020-12-17 22:41:54"
-const Version = "v0.1.5"
+const BuildTime = "2020-12-18 10:52:25"
+const Version = "v0.1.6"
