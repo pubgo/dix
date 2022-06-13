668a4326a1eddde227f1955c94b92d6db7138ab0
百里(barry) <kooksee@163.com>
2022-06-14 00:04:03 +0800

    fix

---
+++ example/lazy/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pubgo/dix"
+	"github.com/pubgo/xerror"
+)
+
+func main() {
+	type handler struct{}
+	dix.Provider(func() *handler {
+		fmt.Println("1")
+		return new(handler)
+	})
+
+	dix.Provider(func() *handler {
+		fmt.Println("2")
+		return new(handler)
+	})
+
+	dix.Provider(func(_ *handler) *xerror.Err {
+		return &xerror.Err{Msg: "ok"}
+	})
+
+	dix.Inject(func(err *xerror.Err) {
+		fmt.Println(err.Msg)
+	})
+}
