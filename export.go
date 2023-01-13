37e68feef847d1d74fa210423c79b5760437cd4d
百里(barry) <kooksee@163.com>
2023-01-13 11:51:54 +0800

    fix

--- export.go
+++ export.go
@@ -1,9 +1,15 @@
 package dix
 
 // New Dix new
-func New(opts ...Option) *Dix                       { return newDix(opts...) }
-func (x *Dix) Provide(param any)                    { x.provide(param) }
-func (x *Dix) Inject(param any, opts ...Option) any { return x.inject(param, opts...) }
+func New(opts ...Option) *Dix { return newDix(opts...) }
+func (x *Dix) Provide(param any) {
+	x.provide(param)
+}
+
+func (x *Dix) Inject(param any, opts ...Option) any {
+	return x.inject(param, opts...)
+}
+
 func (x *Dix) Graph() *Graph {
 	return &Graph{
 		Objects:   x.objectGraph(),
