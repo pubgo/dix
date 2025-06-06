f6ab8d1a830d3932a69db7193ccca248fb44c61e
barry <kooksee@163.com>
2025-06-06 22:24:55 +0800

    fix: barry quick fix, 2025-06-06 22:24:55

--- dixinternal/renderer.go
+++ dixinternal/renderer.go
@@ -19,31 +19,31 @@ func NewDotRenderer() *DotRenderer {
 	}
 }
 
-func (d *DotRenderer) writef(format string, args ...interface{}) {
+func (d *DotRenderer) writeFormat(format string, args ...interface{}) {
 	d.buf.WriteString(d.indent)
 	d.buf.WriteString(fmt.Sprintf(format, args...))
 	d.buf.WriteString("\n")
 }
 
 func (d *DotRenderer) RenderNode(name string, attrs map[string]string) {
-	d.writef("%s [label=\"%s\"%s]", name, name, d.formatAttrs(attrs))
+	d.writeFormat("%s [label=\"%s\"%s]", name, name, d.formatAttrs(attrs))
 }
 
 func (d *DotRenderer) RenderEdge(from, to string, attrs map[string]string) {
-	d.writef("%s -> %s%s", from, to, d.formatAttrs(attrs))
+	d.writeFormat("%s -> %s%s", from, to, d.formatAttrs(attrs))
 }
 
 func (d *DotRenderer) BeginSubgraph(name, label string) {
-	d.writef("subgraph %s {", name)
+	d.writeFormat("subgraph %s {", name)
 	d.indent += "\t"
-	d.writef("label=\"%s\"", label)
+	d.writeFormat("label=\"%s\"", label)
 }
 
 func (d *DotRenderer) EndSubgraph() {
 	if len(d.indent) > 0 {
 		d.indent = d.indent[:len(d.indent)-1]
 	}
-	d.writef("}")
+	d.writeFormat("}")
 }
 
 func (d *DotRenderer) String() string {
@@ -72,7 +72,7 @@ func (d *DotRenderer) formatAttrs(attrs map[string]string) string {
 // RenderProviders 渲染提供者图
 func (d *DotRenderer) RenderProviders(providers map[reflect.Type][]Provider) string {
 	d.buf.Reset()
-	d.writef("digraph G {")
+	d.writeFormat("digraph G {")
 	d.BeginSubgraph("cluster_providers", "providers")
 
 	for providerType, providerList := range providers {
@@ -87,14 +87,14 @@ func (d *DotRenderer) RenderProviders(providers map[reflect.Type][]Provider) str
 	}
 
 	d.EndSubgraph()
-	d.writef("}")
+	d.writeFormat("}")
 	return d.String()
 }
 
 // RenderObjects 渲染对象图
 func (d *DotRenderer) RenderObjects(objects map[reflect.Type]map[string][]reflect.Value) string {
 	d.buf.Reset()
-	d.writef("digraph G {")
+	d.writeFormat("digraph G {")
 	d.BeginSubgraph("cluster_objects", "objects")
 
 	for typ, groups := range objects {
@@ -109,6 +109,6 @@ func (d *DotRenderer) RenderObjects(objects map[reflect.Type]map[string][]reflec
 	}
 
 	d.EndSubgraph()
-	d.writef("}")
+	d.writeFormat("}")
 	return d.String()
 }
